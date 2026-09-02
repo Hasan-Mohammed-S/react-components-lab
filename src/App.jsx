d72b7d48caa0da9b51b6d45cab11e1a56f1763d0
Hasan-Mohammed-S <haasan.mohamedd@gmail.com>
2026-09-02 11:20:26 +0300

    addIconsAndFinesh

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,72 @@
-// src/App.jsx
+import WeatherForecast from './components/WeatherForecast/WeatherForecast';
+import './App.css';
+import sun from './assets/day.svg'; 
+import nightImg from './assets/night.svg';
+import stormy from './assets/stormy.svg';
+import overcast from './assets/cloudy-day.svg';
+import cloudy from './assets/cloudy-night.svg';
 
-const App = () => {
+const weatherForecasts = [
+  {
+    day: 'Mon',
+    img: sun,
+    imgAlt: 'sun icon',
+    conditions: 'sunny',
+    time: 'Morning',
+  },
+  {
+    day: 'Tue',
+    img: nightImg,
+    imgAlt: 'moon icon',
+    conditions: 'clear',
+    time: 'Night',
+  },
+  {
+    day: 'Wed',
+    img: stormy,
+    imgAlt: 'clouds with lightning icon',
+    conditions: 'stormy',
+    time: 'All Day',
+  },
+  {
+    day: 'Thu',
+    img:overcast ,
+   imgAlt: 'sun overcast by clouds icon',
+    conditions: 'overcast',
+    time: 'Evening',
+  },
+  {
+    day: 'Fri',
+    img: cloudy,
+    imgAlt: 'moon overcast by clouds icon',
+    conditions: 'cloudy',
+    time: 'Night',
+  },
+];
 
-  return (
-    <h1>Hello world!</h1>
+
+const App =()=>{
+
+  return(
+<>
+<h1>Local Weather</h1>
+<section>
+{weatherForecasts.map((forecast,index)=>(
+ <WeatherForecast
+ key={index} 
+ day={forecast.day}
+ img={forecast.img}
+ imgAlt={forecast.imgAlt}
+ conditions={forecast.conditions}
+ time={forecast.time}
+   />
+))}
+
+</section>
+</>
+
   );
-}
+};
 
-export default App
+
+export default App;
